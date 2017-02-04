edaf32f14e27b061785d04d78d7e45bbf0440d2b
goran_pds <goran_pds@3451979.no-reply.drupal.org>
2017-02-04 23:09:30 +0100

    Issue #2848313 by gnikolovski: New column

--- src/Plugin/Block/ProjectsStatsBlock.php
+++ src/Plugin/Block/ProjectsStatsBlock.php
@@ -9,6 +9,7 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
 use Drupal\Core\Url;
 use Drupal\Core\Form\FormStateInterface;
+use SimpleXMLElement;
 
 /**
  * Provides a 'ProjectsStatsBlock' block.
@@ -56,6 +57,7 @@ class ProjectsStatsBlock extends BlockBase implements ContainerFactoryPluginInte
   public function defaultConfiguration() {
     return [
       'machine_names' => '',
+      'additional_columns' => [],
       'sort_by' => 'count',
       'cache_age' => 21600,
       'classes' => '',
@@ -73,6 +75,16 @@ class ProjectsStatsBlock extends BlockBase implements ContainerFactoryPluginInte
       '#description' => $this->t('Specify modules/themes by using their machine names. Separate multiple values by a comma.'),
       '#default_value' => $this->configuration['machine_names'],
       '#required' => TRUE,
+    ];
+    $form['additional_columns'] = [
+      '#type' => 'checkboxes',
+      '#title' => $this->t('Additional columns'),
+      '#options' => [
+        'created' => $this->t('Created date'),
+        'changed' => $this->t('Last modified date'),
+        'last_version' => $this->t('Last released version'),
+      ],
+      '#default_value' => $this->configuration['additional_columns'],
     ];
     $form['sort_by'] = [
       '#type' => 'select',
@@ -115,6 +127,7 @@ class ProjectsStatsBlock extends BlockBase implements ContainerFactoryPluginInte
    */
   public function blockSubmit($form, FormStateInterface $form_state) {
     $this->configuration['machine_names'] = $form_state->getValue('machine_names');
+    $this->configuration['additional_columns'] = $form_state->getValue('additional_columns');
     $this->configuration['sort_by'] = $form_state->getValue('sort_by');
     $this->configuration['cache_age'] = $form_state->getValue('cache_age');
     $this->configuration['classes'] = $form_state->getValue('classes');
@@ -126,29 +139,45 @@ class ProjectsStatsBlock extends BlockBase implements ContainerFactoryPluginInte
    */
   public function build() {
     $machine_names = $this->configuration['machine_names'];
+    $machine_names = explode(',', $machine_names);
+    $additional_columns = $this->configuration['additional_columns'];
     $sort_by = $this->configuration['sort_by'];
     $cache_age = $this->configuration['cache_age'];
     $classes = $this->configuration['classes'];
     $target = $this->configuration['target'];
-    $machine_names = explode(',', $machine_names);
-    $stats = [];
+
+    $table_head = [$this->t('Title'), $this->t('Downloads')];
+    foreach ($additional_columns as $key => $value) {
+        if ($value !== 0) {
+            $key = str_replace('_', ' ', $key);
+            $key = ucfirst($key);
+            $key = $this->t($key);
+            $table_head[] = $key;
+        }
+    }
+
+    $table_body = [];
     foreach ($machine_names as $machine_name) {
-      $downloads = $this->performRequest(trim($machine_name));
-      $stats[] = [
+      $stats = $this->getStats(trim($machine_name));
+      $table_body[] = [
         'title' => ucfirst(str_replace('_', ' ', trim($machine_name))),
         'url' => Url::fromUri('https://www.drupal.org/project/' . trim($machine_name)),
-        'downloads' => number_format($downloads, 0, '.', ','),
-        'downloads_raw' => $downloads,
+        'downloads' => number_format($stats['download_count'], 0, '.', ','),
+        'downloads_raw' => $stats['download_count'],
+        'created' => $additional_columns['created'] !== 0 ? $stats['created'] : NULL,
+        'changed' => $additional_columns['changed'] !== 0 ? $stats['changed'] : NULL,
+        'last_version' => $additional_columns['last_version'] !== 0 ? $stats['last_version'] : NULL,
       ];
     }
 
     if ($sort_by != 'no') {
-      usort($stats, [$this, 'sortModulesList']);
+      usort($table_body, [$this, 'sortModulesList']);
     }
 
     return [
       '#theme' => 'projects_stats',
-      '#stats' => $stats,
+      '#table_head' => $table_head,
+      '#table_body' => $table_body,
       '#target' => $target == TRUE ? '_blank' : '_self',
       '#classes' => $classes,
       '#cache' => ['max-age' => $cache_age],
@@ -158,23 +187,57 @@ class ProjectsStatsBlock extends BlockBase implements ContainerFactoryPluginInte
   /**
    * Get data from drupal.org API endpoint.
    */
-  private function performRequest($machine_names) {
+  private function getStats($machine_name) {
     $base_url = 'https://www.drupal.org/api-d7/node.json?field_project_machine_name=';
     $client = new Client();
     try {
-      $res = $client->get($base_url . $machine_names, ['http_errors' => FALSE]);
+      $res = $client->get($base_url . $machine_name, ['http_errors' => FALSE]);
       $body = $res->getBody();
       $decoded_body = json_decode($body, TRUE);
       if (!isset($decoded_body['list'][0])) {
         return 0;
       }
       $download_count = $decoded_body['list'][0]['field_download_count'];
-      $download_count = isset($download_count) ? $download_count : 0;
-      return $download_count;
+      $created = $decoded_body['list'][0]['created'];
+      $version_data = $this->getLastVersion($machine_name);
+      $changed = $version_data['changed'];
+      $last_version = $version_data['last_version'];
+      $stats = [
+        'download_count' => $download_count,
+        'created' => date('d-m-Y', $created),
+        'changed' => $changed,
+        'last_version' => $last_version,
+      ];
+      return $stats;
     }
     catch (RequestException $e) {
       drupal_set_message($e);
-      return 0;
+      $stats = [
+        'download_count' => 0,
+        'created' => 'n/a',
+        'changed' => 'n/a',
+        'last_version' => 'n/a',
+      ];
+      return $stats;
+    }
+  }
+
+  /**
+   * Get release data from drupal.org API endpoint.
+   */
+  private function getLastVersion($machine_name) {
+    $client = new Client();
+    try {
+      $res = $client->get("https://updates.drupal.org/release-history/$machine_name/all", ['http_errors' => FALSE]);
+      $xml = $res->getBody()->getContents();
+      $versions = new SimpleXMLElement($xml);
+      $last_version = isset($versions->releases->release->version) ? $versions->releases->release->version : 'n/a';
+      $changed = isset($versions->releases->release->date) ? date('d-m-Y', $versions->releases->release->date->__toString()) : 'n/a';
+      return ['last_version' => $last_version, 'changed' => $changed];
+    }
+    catch (RequestException $e) {
+      drupal_set_message($e);
+      return ['last_version' => 'n/a', 'changed' => 'n/a'];
     }
   }
 
